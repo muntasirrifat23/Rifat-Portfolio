cc8ef34748fdca994238d1b0f732d4534fe3b719
Munshi Muntasir Rifat <rifat2002m@gmail.com>
2023-07-02 11:14:49 +0600

    fixed nav

--- src/Component/Shared/Nav.jsx
+++ src/Component/Shared/Nav.jsx
@@ -6,14 +6,16 @@ const Nav = () => {
             <div className="navbar text-white bg-base-800">
                 
                 <div className="navbar-start">
-                    <div className="flex justify-center items-center text-center">
-                    <div className="btm-nav text-black w-2/3 navbar bg-black p-5 fixed mb-15 z-10 bg-opacity-70 text-white bottom-0 rounded-xl text-xl lg:hidden flex justify-center items-center text-center">
+                    {/* <div className="flex justify-center items-center text-center"> */}
+                    {/* <div className="btm-nav text-black w-2/3 navbar bg-black p-5 fixed mb-15 z-10 bg-opacity-70 text-white bottom-0 rounded-xl text-xl lg:hidden flex justify-center items-center text-center"> */}
                         {/* small */}  
-                        <a className='ml-3 mt-2' href='#header'><AiOutlineHome /></a>
-                        <a className='ml-3 mt-2' href='#project'><AiOutlineProject /></a>
-                        <a className='ml-3 mt-2' href='#mySelf'><AiOutlineUser /></a>
-                        <a className='ml-3 mt-2' href='#contact'> <AiOutlineMessage /></a>
-                    </div>
+                        <div className="navbar-center p-5 mb-15 text-white flex justify-center items-center fixed z-10 w-full text-white bottom-0 rounded-xl">
+                        <ul className="menu menu-horizontal px-1 text-2xl text-white bg-slate-800 rounded-xl bg-opacity-80 font-bold">
+                        <a className='ml-10 mt-2' href='#header'><AiOutlineHome /></a>
+                        <a className='ml-10 mt-2' href='#project'><AiOutlineProject /></a>
+                        <a className='ml-10 mt-2' href='#mySelf'><AiOutlineUser /></a>
+                        <a className='ml-10 mr-10 mt-2 mb-2' href='#contact'> <AiOutlineMessage /></a>
+                    </ul>
                     </div>
                    
 
@@ -24,12 +26,12 @@ const Nav = () => {
                 </div>
                 {/* big */}
                 
-                <div className="navbar-center hidden lg:flex top-0 p-5  mb-15 z-10 bg-opacity-70 text-white flex justify-center items-center">
-                    <ul className="menu menu-horizontal px-1 text-xl text-white bg-slate-800">
+                <div className="navbar-center hidden lg:flex top-0 p-5  mb-15 z-10  text-white flex justify-center items-center fixed z-10 w-full text-white top-0 rounded-xl">
+                    <ul className="menu menu-horizontal px-1 text-xl text-white bg-slate-800 rounded-xl bg-opacity-60">
                         <a className='ml-3 mt-2' href='#header'> Home</a>
                         <a className='ml-3 mt-2' href='#project'>Project</a>
                         <a className='ml-3 mt-2' href='#mySelf'>My-Self</a>
-                        <a className='ml-3 mt-2' href='#contact'> Contact</a>
+                        <a className='ml-3 mt-2 mr-3' href='#contact'> Contact</a>
                     </ul>
                 </div>
 
